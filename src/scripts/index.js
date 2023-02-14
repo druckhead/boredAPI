4b2f1065b0391e64274d384dcc4d1da187349d78
druckhead <daniel32raz@gmail.com>
2023-02-14 12:53:52 +0200

    enable adding items to list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -40,7 +40,41 @@ const getActivity = (event) => {
     fetch(url)
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
+            const alert = document.getElementById(`errorAlert`)
+            if (`error` in json) {
+                alert.classList.remove(`alert-success`)
+                alert.classList.add(`alert-danger`)
+                alert.innerText = `No activity found with these filters, please try again.`
+                alert.classList.remove(`d-none`)
+            } else {
+                alert.classList.remove(`alert-danger`)
+                alert.classList.add(`alert-success`)
+                alert.innerText = `Activity added successfuly!`
+                alert.classList.remove(`d-none`)
+
+                const item = document.createElement(`li`)
+                const text = document.createElement(`p`)
+                const deleteButton = document.createElement(`button`)
+
+                text.setAttribute('class', 'h6 d-inline m-0')
+                deleteButton.setAttribute('type', 'button')
+                deleteButton.setAttribute('aria-label', 'Close')
+                deleteButton.setAttribute('class', 'btn btn-outline-danger btn-sm')
+                deleteButton.innerHTML = 'X'
+                deleteButton.setAttribute('onclick', 'removeItemFromList(this)')
+
+                item.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center")
+                if (json.price > 0.0) {
+                    const times = json.price * 10.0 === 0 ? 1 : json.price * 10.0
+                    text.innerHTML = `${json.activity}, up to ${json.participants} people. Price: ${`$`.repeat(times)}`
+                } else {
+                    text.innerHTML = `${json.activity}, up to ${json.participants} people.`
+                }
+                item.appendChild(text)
+                item.appendChild(deleteButton)
+                const activityList = document.getElementById("activities");
+                activityList.insertBefore(item, activityList.firstChild);
+            }
         })
         .then(() => {
             submitBtn.removeChild(loadingSpan)
@@ -84,4 +118,20 @@ const getRandomActivity = () => {
         .catch((error) => {
             console.log(error);
         })
+}
+
+const clearAll = () => {
+    if (confirm(`Are you sure? This can't be undone.`)) {
+        const activityList = document.getElementById('activities')
+        let first = activityList.firstChild
+        while (first) {
+            first.remove()
+            first = activityList.firstChild
+        }
+    }
+}
+
+function removeItemFromList(btn) {
+    parentItem = btn.parentNode
+    parentItem.remove()
 }
